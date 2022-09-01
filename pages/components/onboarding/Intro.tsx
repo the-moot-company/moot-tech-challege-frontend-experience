4f98595d69f4c1607f7c55ffa5b34e2e8618a563
Alex Marriott <alex.marriott@buildcircle.co.uk>
2022-09-01 16:13:18 +0100

    Implement intro page html/logic, styling WIP

--- pages/components/onboarding/Intro.tsx
+++ pages/components/onboarding/Intro.tsx
@@ -1,3 +1,30 @@
-export default function Intro() {
-  return <p>intro</p>
+import React from "react";
+import Image from "next/image";
+import ButtonLarge from "../common/ButtonLarge";
+
+interface IntroProps {
+  nextStep: () => void;
 }
+
+export default function Intro({nextStep}: IntroProps) {
+  return (
+    <div className="sm:max-w-xl flex flex-col justify-center items-center">
+      <Image
+        src="/images/logo.png"
+        alt="Moot Logo"
+        width={131}
+        height={131}
+        className=""
+      />
+      <h1 className="text-4xl font-extrabold text-gray-700 tracking-tight sm:text-5xl">
+        Welcome to Moot
+      </h1>
+      <p className="mt-4 text-xl text-gray-500 ">
+        Moot is a work hub that makes remote work multiplayer, so you and your team can collaborate better, faster and
+        cheaper.
+      </p>
+      <ButtonLarge onClick={nextStep} text={"Continue"}/>
+    </div>
+  )
+}
+
